b1fdd9ae549e8ef39168e734c6ff0e43ba1d5058
kkakama <sallkakama96@yahoo.fr>
2017-12-12 12:43:27 +0100

    Creation of type's enum Implementation of the classe WebServiceDescription To do : Json

---
+++ src/main/java/io/github/oliviercailloux/y2017/WebServiceDescription.java
@@ -0,0 +1,68 @@
+package io.github.oliviercailloux.y2017;
+
+import java.io.FileReader;
+import java.util.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+/**
+ * 
+ * @author KAKAMA & SAMI
+ *
+ */
+public class WebServiceDescription {
+	private  String id;
+	XmcdaModularTypes type;
+	private static Map<String, XmcdaModularTypes> map =new HashMap<String, XmcdaModularTypes>(); 
+	
+
+	public WebServiceDescription(String id, XmcdaModularTypes type) {
+		this.id = id;
+		this.type = type;
+		map.put(id, type);
+		
+	}
+	/**
+	 * This function has in parameters the id of 
+	 * the pair and associate a type to it
+	 * @param id
+	 * 			the id of the pair
+	 * @param type
+	 * 			the type
+	 */
+	public void insertPair(String id,XmcdaModularTypes type){
+		map.put(id, type);
+	}
+	public void removeFromMap(String id){
+		map.remove(id);
+	}
+	// getters and setters
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public XmcdaModularTypes getType() {
+		return type;
+	}
+	public void setType(XmcdaModularTypes type) {
+		this.type = type;
+	}
+	public  Map<String, XmcdaModularTypes> getMap() {
+		return map;
+	}
+	public  void setMap(Map<String, XmcdaModularTypes> map) {
+		WebServiceDescription.map = map;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		XmcdaModularTypes t = null; 
+		WebServiceDescription d  = new WebServiceDescription("id1", t.affineLinearFunction);
+		WebServiceDescription.map.put("id2", XmcdaModularTypes.binaryMeasurement);
+		System.out.println(WebServiceDescription.map.get("id1"));
+		System.out.println(WebServiceDescription.map.get("id2"));
+	}
+
+}
---
+++ src/main/java/io/github/oliviercailloux/y2017/XmcdaModularTypes.java
@@ -0,0 +1,166 @@
+package io.github.oliviercailloux.y2017;
+
+/**
+ * 
+ * @author KAKAMA ET SAMI 
+ *
+ */
+public enum XmcdaModularTypes {
+	DefaultX2{
+		
+	},
+	 Function{
+
+		public String descr() {
+			return "type Fonction !";
+		}
+	},
+	ExponentialFunction {
+	
+	},
+	Point2D {
+		
+	},
+	PiecewiseLinearFunctio{
+		
+	},
+	affineLinearFunction{
+		
+	},
+	keyedEntity{
+		
+	},
+	alternative{
+		
+	},
+	alternativeSet{
+		
+	},
+	category{
+		
+	},
+	categorySet{
+		
+	},
+	attribute{
+		
+	},
+	attributeSet{
+		
+	},
+	nominalToCardinal{
+		
+	},
+	nominalToCardinalSet{
+		
+	},
+	measurement{
+		
+	},
+	exactMeasurement{
+		
+	},
+	binaryMeasurement{
+		
+	},
+	interval{
+		
+	},
+	gaussian{
+		
+	},
+	impreciseNominalMeasurement{
+		
+	},
+	criterion{
+		
+	},
+	criterionSet{
+		
+	},
+	directUtilityCriterion{
+		
+	},
+	directUtilityCriterionSet{
+		
+	},
+	nominalUtilityCriterion{
+		
+	},
+	
+	nominalUtilityCriterionSet{
+		
+	},
+	cardinalUtilityCriterion{
+		
+	},
+	cardinalUtilityCriterionSet{
+		
+	},
+	directedCriterion{
+		
+	},
+	directedCriterionSet{
+		
+	},
+	outrankingCriterionSet{
+		
+	},
+	valuedEntity{
+		
+	},
+	valuedEntitySet{
+		
+	},
+	exactValuedEntity{
+		
+	},
+	exactValuedEntitySet{
+		
+	},
+	anyValuedPair{
+		
+	},
+	anyValuedRelation{
+		
+	},
+	binaryValuedPair{
+		
+	},
+	binaryRelation{
+		
+	},
+	valuedPair{
+		
+	},
+	valuedRelation{
+		
+	},
+	intervalValuedPair{
+		
+	},
+	intervalValuedRelation{
+		
+	},
+	
+	gaussianValuedPair{
+		
+	},
+	gaussianValuedRelation{
+		
+	},
+	nominalValuedPair{
+		
+	},
+	nominalValuedRelation{
+		
+	},
+	impreciseNominalValuedPair{
+		
+	},
+	impreciseNominalValuedRelation{
+		
+	};
+	
+
+}
