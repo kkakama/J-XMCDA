e912bfc42ee222b6e62253615aa6ed1c3ac551db
BencherifNoufissa <33414915+BencherifNoufissa@users.noreply.github.com>
2018-02-18 16:38:26 +0100

    Update EvaluationTest.java

--- src/test/java/io/github/oliviercailloux/y2017/entities/EvaluationTest.java
+++ src/test/java/io/github/oliviercailloux/y2017/entities/EvaluationTest.java
@@ -7,11 +7,20 @@ public class EvaluationTest {
 
 	@Test
 	public void test() {
-		
-		Criterion crit0= new Criterion(1,"c");
-		//Alternative alt0 = new Alternative(1,"a");
-		//Evaluation eval = new Evaluation(alt0, crit0, 1.0);
-		//System.out.println(eval.toJson());
+				Criterion crit0= new Criterion(1,"c");
+		Alternative alt0 = new Alternative("1","a");
+		Evaluation eval = new Evaluation(alt0, crit0, 1.0);
+		String evalJson = eval.toJson();
+		evalJson.equals("{\r\n" + 
+				"    \"alt\": {\r\n" + 
+				"        \"id\": \"1\",\r\n" + 
+				"        \"name\": \"a\"\r\n" + 
+				"    },\r\n" + 
+				"    \"crit\": {\r\n" + 
+				"        \"id\": 1,\r\n" + 
+				"        \"name\": \"c\"\r\n" + 
+				"    },\r\n" + 
+				"    \"val\": 1.0\r\n" + "}");
 		
 	}
 
